317d3e40fb2897bd0ecb05a1fc29211c0da7d29e
Дубровин Александр <aleksandr.dubrovin@osinit.com>
2020-10-09 21:43:32 +0300

    Готовый вариант автоматизированного обращения

---
+++ tasks.py
@@ -0,0 +1,12 @@
+from celery import Celery
+from celery.schedules import crontab
+from server import obtain_mail
+
+
+celery_app = Celery('tasks', broker='redis://localhost:6379/0')
+
+
+@celery_app.on_after_configure.connect
+def periodic_mail_receive(sender, **kwargs):
+    sender.add_periodic_task(crontab(minute='*/5'), obtain_mail.s())
+
