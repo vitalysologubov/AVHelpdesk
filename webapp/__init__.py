349c7b2424540edc0f6519fcd6cb5fa355435163
Дубровин Александр <aleksandr.dubrovin@osinit.com>
2020-10-11 19:30:37 +0300

    Rollback after bad merge

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import flash, Flask, redirect, render_template, url_for
 from flask_migrate import Migrate
 from webapp.add_tickets import add_ticket
 from webapp.av_mail import fetch_mail
+from webapp.forms import SendForm
 from webapp.models import db
+from webapp.send_email import send_email
 
 
 def create_app():
@@ -16,11 +18,33 @@ def create_app():
 
     @app.route('/')
     def index():
-        """Главная страница"""
+        """Главная страница: обработка входящих писем"""
 
         messages = fetch_mail()
         add_ticket(messages)
 
         return 'Работаем!'
+
+    @app.route('/send')
+    def email_form():
+        """Форма отправки email"""
+
+        form = SendForm()
+        return render_template('email_form.html', title='Отправка email', form=form)
+
+    @app.route('/email_proccess', methods=['POST'])
+    def email_proccess():
+        """Процесс отправки email"""
+
+        form = SendForm()
+        subject = 'Тестовая тема'
+        message = 'Тестовое сообщение'
+
+        if form.validate_on_submit():
+            receiver = form.email.data
+            message = send_email(receiver, subject, message)
+            flash(message)
+
+            return redirect(url_for('email_form'))
 
     return app
